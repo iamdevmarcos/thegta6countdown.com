678887ba46bb27e904a290df8145e374247983b2
Marcos Mendes <marcosdev.me@gmail.com>
2024-11-20 11:38:57 -0300

    feat: implements github corner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,19 @@ import { CountdownTimer } from '@/components/countdownTimer'
 import { Footer } from '@/components/footer'
 import { HeaderTitle } from '@/components/headerTitle'
 import { TrailerPreview } from '@/components/trailerPreview'
+import GithubCorner from 'react-github-corner'
 
 export default function Home() {
   return (
     <BackgroundWrapper>
+      <GithubCorner
+        href={'https://github.com/iamdevmarcos/thegta6countdown.com'}
+        target="_blank"
+        bannerColor="#FFBF76"
+        octoColor="#B11778"
+        size={80}
+        direction="right"
+      />
       <div className="flex flex-col items-center justify-center gap-12">
         <div className="bg-[#151515] backdrop-blur-sm bg-opacity-90 flex flex-col px-14 py-4 gap-5 items-center justify-center shadow-custom">
           <HeaderTitle />
