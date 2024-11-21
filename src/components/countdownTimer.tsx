57f8aa46f2a6cd89ff5c9bc633636fdd5009cf99
João Nogueira <147201184+joaoNogueiradev@users.noreply.github.com>
2024-11-21 20:11:55 -0300

    Refactor timer context and add release date info
    * add context to the code
    
    * adding new text content to footer

--- src/components/countdownTimer.tsx
+++ src/components/countdownTimer.tsx
@@ -1,41 +1,9 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react'
-
-interface TimeUnit {
-  label: string
-  value: string
-}
+import { useTimer } from "@/contexts/timerContext";
 
 export const CountdownTimer = () => {
-  const targetDate = new Date('2025-11-30T00:00:00')
-
-  const [timeLeft, setTimeLeft] = useState(() => {
-    const now = new Date()
-    return Math.floor((targetDate.getTime() - now.getTime()) / 1000)
-  })
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTimeLeft((prev) => (prev > 0 ? prev - 1 : 0))
-    }, 1000)
-
-    return () => clearInterval(interval)
-  }, [])
-
-  const timeDay = 24
-
-  const days = Math.floor(timeLeft / (timeDay * 60 * 60))
-  const hours = Math.floor((timeLeft % (timeDay * 60 * 60)) / (60 * 60))
-  const minutes = Math.floor((timeLeft % (60 * 60)) / 60)
-  const seconds = timeLeft % 60
-
-  const timeUnits: TimeUnit[] = [
-    { label: 'Days', value: `${String(days).padStart(2, '0')} :` },
-    { label: 'Hours', value: `${String(hours).padStart(2, '0')} :` },
-    { label: 'Minutes', value: `${String(minutes).padStart(2, '0')} :` },
-    { label: 'Seconds', value: `${String(seconds).padStart(2, '0')}` },
-  ]
+  const { timeUnits } = useTimer();
 
   return (
     <div className="flex justify-center items-center space-x-8 pb-6">
@@ -50,5 +18,5 @@ export const CountdownTimer = () => {
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,9 @@
 export const Footer = () => (
   <div className="mb-5 bg-[#151515] backdrop-blur-sm bg-opacity-90 flex flex-col items-center justify-center shadow-secondary text-center px-6 py-4 gap-2 font-poppins tracking-tightest">
     <p className="text-[14px] bg-secondary-gradient bg-clip-text text-transparent">
+      Expected to release between September and November 2025
+    </p>
+    <p className="text-[14px] bg-secondary-gradient bg-clip-text text-transparent">
       All trademarks, images, and content related to GTA 6 are the <br />
       property of Rockstar Games and their respective owners.
     </p>
@@ -8,4 +11,4 @@ export const Footer = () => (
       Made by excited fans for fans 🩵
     </p>
   </div>
-)
+);
